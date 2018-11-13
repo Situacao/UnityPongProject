b2b77030bde8015abed95483e5949052a26bbae0
JV <jpvinagre6@gmail.com>
2018-11-13 10:59:56 +0000

    Aula 13/11

---
+++ Assets/Scripts/Physics/CameraShake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    public IEnumerator CameraShakeMethod(float _duration, float _amount)
+    {
+        //yield return new WaitForSeconds(3f);
+        float startTime = Time.time;
+        Vector3 startPos = transform.position;
+
+        while (Time.time < startTime + _duration)
+        {
+            float newPosX = Random.Range(-1f, 1f) * _amount;
+            float newPosY = Random.Range(-1f, 1f) * _amount;
+            Vector3 newPos = new Vector3(newPosX, newPosY);
+            Vector3 finalPos = startPos + newPos * Time.deltaTime;
+
+            transform.position = finalPos;
+
+            // Este return vai fazer com que o while passe por vários frames
+            yield return null;
+        }
+        transform.position = startPos;
+
+    }
+}
---
+++ Assets/Scripts/Physics/CubeCol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCol : MonoBehaviour {
+
+    private CameraShake camShake;
+
+    [SerializeField]
+    private AudioSource colAudio;
+
+    private void Awake()
+    {
+        camShake = GameObject.Find("Main Camera").GetComponent<CameraShake>();
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("TIQUINHO SOARES");
+        colAudio.Play();
+        StartCoroutine(camShake.CameraShakeMethod(0.5f, 8f));
+    }
+}
