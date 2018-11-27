02e44451d4378ad16441f1e14155823c936a9887
JV <jpvinagre6@gmail.com>
2018-11-27 11:03:10 +0000

    Lesson 27/11

---
+++ Assets/Scripts/UI/MainMenuUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUIManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject MatchScreenGO;
+
+    public void StartGameMethod()
+    {
+        MatchScreenGO.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void QuitGameMethod()
+    {
+        Application.Quit();
+    }
+}
