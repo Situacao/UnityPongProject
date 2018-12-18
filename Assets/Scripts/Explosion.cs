a0c15755e25a3d212941f8d1e312dc1d48296b0f
JV <jpvinagre6@gmail.com>
2018-12-18 10:55:42 +0000

    Lesson 18/12

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public ParticleSystem Main;
+    public ParticleSystem[] Ring;
+    public SparkParticles Sparks;
+    public ParticleSystem Flash;
+    public ParticleSystem[] Debris;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Play();
+        }
+    }
+
+    public void Play()
+    {
+        Main.Play();
+        for (int i = 0; i < Ring.Length; i++)
+            Ring[i].Play();
+        Sparks.Play();
+        Flash.Play();
+        for (int i = 0; i < Debris.Length; i++)
+            Debris[i].Play();
+    }
+}
+
